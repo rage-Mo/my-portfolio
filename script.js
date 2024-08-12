0b51db3d335b31c8f01254fb36f3e94f9eaa2439
rage-Mo <69406091+rage-Mo@users.noreply.github.com>
2024-08-12 14:01:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,20 @@ function showPopup(bool) {
     document.getElementById('popup').style.visibility = 'hidden'
   }
 }
+
+
+function updateTime() {
+    var date = new Date();
+    var time = date.toLocaleTimeString();
+
+    // Set the value of the textbox with id "current-time"
+    document.getElementById('current-time').value = time;
+
+    // Call requestAnimationFrame again to keep the time updated
+    requestAnimationFrame(updateTime);
+}
+
+// Start the time updates
+window.onload = function() {
+    updateTime();
+};
